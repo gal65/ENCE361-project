dee1a6f2a6308433a55edc5f6dfead4d821d3e10
simon <sja200@uclive.ac.nz>
2020-05-17 14:59:24 +1200

    Slightly more responsive, debounce working

--- 361Project/buttons4.c
+++ 361Project/buttons4.c
@@ -31,45 +31,43 @@ static uint8_t cycles = 0; //checks the cycles for the button held down
 
 // *******************************************************
 
-
-
 // initButtons: Initialise the variables associated with the set of buttons
 // defined by the constants in the buttons2.h header file.
-void initButtons (void)
+void initButtons(void)
 {
 
     int i;
 
     // UP button (active HIGH)
-    SysCtlPeripheralEnable (UP_BUT_PERIPH);
-    GPIOPinTypeGPIOInput (UP_BUT_PORT_BASE, UP_BUT_PIN);
-    GPIOPadConfigSet (UP_BUT_PORT_BASE, UP_BUT_PIN, GPIO_STRENGTH_2MA,
-       GPIO_PIN_TYPE_STD_WPD);
+    SysCtlPeripheralEnable(UP_BUT_PERIPH);
+    GPIOPinTypeGPIOInput(UP_BUT_PORT_BASE, UP_BUT_PIN);
+    GPIOPadConfigSet(UP_BUT_PORT_BASE, UP_BUT_PIN, GPIO_STRENGTH_2MA,
+    GPIO_PIN_TYPE_STD_WPD);
     but_normal[UP] = UP_BUT_NORMAL;
     // DOWN button (active HIGH)
-    SysCtlPeripheralEnable (DOWN_BUT_PERIPH);
-    GPIOPinTypeGPIOInput (DOWN_BUT_PORT_BASE, DOWN_BUT_PIN);
-    GPIOPadConfigSet (DOWN_BUT_PORT_BASE, DOWN_BUT_PIN, GPIO_STRENGTH_2MA,
-       GPIO_PIN_TYPE_STD_WPD);
+    SysCtlPeripheralEnable(DOWN_BUT_PERIPH);
+    GPIOPinTypeGPIOInput(DOWN_BUT_PORT_BASE, DOWN_BUT_PIN);
+    GPIOPadConfigSet(DOWN_BUT_PORT_BASE, DOWN_BUT_PIN, GPIO_STRENGTH_2MA,
+    GPIO_PIN_TYPE_STD_WPD);
     but_normal[DOWN] = DOWN_BUT_NORMAL;
     // LEFT button (active LOW)
-    SysCtlPeripheralEnable (LEFT_BUT_PERIPH);
-    GPIOPinTypeGPIOInput (LEFT_BUT_PORT_BASE, LEFT_BUT_PIN);
-    GPIOPadConfigSet (LEFT_BUT_PORT_BASE, LEFT_BUT_PIN, GPIO_STRENGTH_2MA,
-       GPIO_PIN_TYPE_STD_WPU);
+    SysCtlPeripheralEnable(LEFT_BUT_PERIPH);
+    GPIOPinTypeGPIOInput(LEFT_BUT_PORT_BASE, LEFT_BUT_PIN);
+    GPIOPadConfigSet(LEFT_BUT_PORT_BASE, LEFT_BUT_PIN, GPIO_STRENGTH_2MA,
+    GPIO_PIN_TYPE_STD_WPU);
     but_normal[LEFT] = LEFT_BUT_NORMAL;
     // RIGHT button (active LOW)
-      // Note that PF0 is one of a handful of GPIO pins that need to be
-      // "unlocked" before they can be reconfigured.  This also requires
-      //      #include "inc/tm4c123gh6pm.h"
-    SysCtlPeripheralEnable (RIGHT_BUT_PERIPH);
+    // Note that PF0 is one of a handful of GPIO pins that need to be
+    // "unlocked" before they can be reconfigured.  This also requires
+    //      #include "inc/tm4c123gh6pm.h"
+    SysCtlPeripheralEnable(RIGHT_BUT_PERIPH);
     //---Unlock PF0 for the right button:
     GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
     GPIO_PORTF_CR_R |= GPIO_PIN_0; //PF0 unlocked
     GPIO_PORTF_LOCK_R = GPIO_LOCK_M;
-    GPIOPinTypeGPIOInput (RIGHT_BUT_PORT_BASE, RIGHT_BUT_PIN);
-    GPIOPadConfigSet (RIGHT_BUT_PORT_BASE, RIGHT_BUT_PIN, GPIO_STRENGTH_2MA,
-       GPIO_PIN_TYPE_STD_WPU);
+    GPIOPinTypeGPIOInput(RIGHT_BUT_PORT_BASE, RIGHT_BUT_PIN);
+    GPIOPadConfigSet(RIGHT_BUT_PORT_BASE, RIGHT_BUT_PIN, GPIO_STRENGTH_2MA,
+    GPIO_PIN_TYPE_STD_WPU);
     but_normal[RIGHT] = RIGHT_BUT_NORMAL;
 
     for (i = 0; i < NUM_BUTS; i++)
@@ -98,30 +96,27 @@ void initButtons (void)
 
 }
 
-
-void UPButIntHandler (void)
+void UPButIntHandler(void)
 {
-    flagU = 1;      // maybe add an if-condition that checks the button is still pressed as a rough kind of debounce?
+    flagU = 1;
     GPIOIntClear(UP_BUT_PORT_BASE, UP_BUT_PIN);
 }
 
-
-void DOWNButIntHandler (void)
+void DOWNButIntHandler(void)
 {
     flagD = 1;
     GPIOIntClear(DOWN_BUT_PORT_BASE, DOWN_BUT_PIN);
 }
 
 // Left and Right button interrupts are on the same port so they occur at the same time
-void LEFTButIntHandler (void)
+void LEFTButIntHandler(void)
 {
     flagL = 1;
     GPIOIntClear(LEFT_BUT_PORT_BASE, LEFT_BUT_PIN);
     GPIOIntClear(RIGHT_BUT_PORT_BASE, RIGHT_BUT_PIN);
 }
 
-
-void RIGHTButIntHandler (void)
+void RIGHTButIntHandler(void)
 {
     flagR = 1;
     GPIOIntClear(RIGHT_BUT_PORT_BASE, RIGHT_BUT_PIN);
@@ -137,16 +132,19 @@ void RIGHTButIntHandler (void)
 // A state change occurs only after NUM_BUT_POLLS consecutive polls have
 // read the pin in the opposite condition, before the state changes and
 // a flag is set.  Set NUM_BUT_POLLS according to the polling rate.
-void updateButtons (void)
+void updateButtons(void)
 {
     bool but_value[NUM_BUTS];
     int i;
 
     // Read the pins; true means HIGH, false means LOW
-    but_value[UP] = (GPIOPinRead (UP_BUT_PORT_BASE, UP_BUT_PIN) == UP_BUT_PIN);
-    but_value[DOWN] = (GPIOPinRead (DOWN_BUT_PORT_BASE, DOWN_BUT_PIN) == DOWN_BUT_PIN);
-    but_value[LEFT] = (GPIOPinRead (LEFT_BUT_PORT_BASE, LEFT_BUT_PIN) == LEFT_BUT_PIN);
-    but_value[RIGHT] = (GPIOPinRead (RIGHT_BUT_PORT_BASE, RIGHT_BUT_PIN) == RIGHT_BUT_PIN);
+    but_value[UP] = (GPIOPinRead(UP_BUT_PORT_BASE, UP_BUT_PIN) == UP_BUT_PIN);
+    but_value[DOWN] = (GPIOPinRead(DOWN_BUT_PORT_BASE, DOWN_BUT_PIN)
+            == DOWN_BUT_PIN);
+    but_value[LEFT] = (GPIOPinRead(LEFT_BUT_PORT_BASE, LEFT_BUT_PIN)
+            == LEFT_BUT_PIN);
+    but_value[RIGHT] = (GPIOPinRead(RIGHT_BUT_PORT_BASE, RIGHT_BUT_PIN)
+            == RIGHT_BUT_PIN);
 
     // Iterate through the buttons, updating button variables as required
     for (i = 0; i < NUM_BUTS; i++)
@@ -166,12 +164,11 @@ void updateButtons (void)
     }
 }
 
-
 // *******************************************************
 // checkButton: Function returns the new button logical state if the button
 // logical state (PUSHED or RELEASED) has changed since the last call,
 // otherwise returns NO_CHANGE.
-uint8_t checkButton (uint8_t butName)
+uint8_t checkButton(uint8_t butName)
 {
     if (but_flag[butName])
     {
@@ -184,97 +181,40 @@ uint8_t checkButton (uint8_t butName)
     return NO_CHANGE;
 }
 
-
 // Function to poll buttons; pushing UP will increment the mode cycle, pushing DOWN will set a flag to logic high
 // Only polls the flags set by the interrupts and does not require updating the buttons to do so
 vector_inputs readButtonFlags(vector_inputs inputFlags)
 {
-    if (flagU == 1) {
+    if (flagU == 1)
+    {
         inputFlags.U = 1;
         flagU = 0;
     }
 
-    if (flagD == 1) {
+    if (flagD == 1)
+    {
         inputFlags.D = 1;
         flagD = 0;
     }
 
-    if (flagL == 1) {
+    if (flagL == 1)
+    {
         inputFlags.L = 1;
         flagL = 0;
     }
 
-    if (flagR == 1) {
+    if (flagR == 1)
+    {
         inputFlags.R = 1;
         flagR = 0;
     }
-
-    /*
-    uint8_t butState;
-    int flag = 0;
-
-    // Poll the buttons
-    updateButtons();
-
-    // check state of each button
-    butState = checkButton(UP);
-    switch (butState)
-    {
-    case PUSHED: // increment dispMode cycler if a change is detected on UP button
-        if (dispMode == MPS) {
-            dispMode = RAW;
-        } else {
-            dispMode++;
-        }
-        break;
-        // Do nothing if state is NO_CHANGE or RELEASED
-    }
-    butState = checkButton(DOWN);
-    switch (butState)
-    {
-    case PUSHED: // set flag to logic high if a change is detected on DOWN button
-        flag = 1;
-        break;
-        // Do nothing if state is NO_CHANGE or RELEASED
-    }
-    */
-
-    /*
-     butState = checkButton(LEFT);
-     switch (butState)
-     {
-     case PUSHED:
-     displayButtonState("LEFT ", "PUSH", ++leftPushes, 2);
-     break;
-     case RELEASED:
-     displayButtonState("LEFT ", "RELS", leftPushes, 2);
-     break;
-     // Do nothing if state is NO_CHANGE
-     }
-     butState = checkButton(RIGHT);
-     switch (butState)
-     {
-     case PUSHED:
-     displayButtonState("RIGHT", "PUSH", ++rightPushes, 3);
-     break;
-     case RELEASED:
-     displayButtonState("RIGHT", "RELS", rightPushes, 3);
-     break;
-     // Do nothing if state is NO_CHANGE
-     }
-     */
-
-
-    // set each field of the vector_buttons    - Better to do this as they happen?
-//    button_flags.dispMode = dispMode;
-//    button_flags.D = flag;
 
     return inputFlags;
 }
 
 // Function acts as a detector for the a button held down
 // Checks how long a button is held down depending on a limit
-int detect_hold(uint8_t butName,int lim)
+int detect_hold(uint8_t butName, int lim)
 {
     int fin_flag = 0;
     uint8_t butState;
@@ -284,11 +224,13 @@ int detect_hold(uint8_t butName,int lim)
     {
         cycles++;
     }
-    else {
-            flagD = 0;
-            cycles = 0;
-        }
-    if (cycles == lim){
+    else
+    {
+        flagD = 0;
+        cycles = 0;
+    }
+    if (cycles == lim)
+    {
         fin_flag = 1;
         cycles = 0;
     }
@@ -300,19 +242,26 @@ int detect_hold(uint8_t butName,int lim)
 uint8_t swap_disp(uint8_t dispmode, uint8_t dir)
 {
     uint8_t next_disp;
-    switch(dir){
+    switch (dir)
+    {
     case 0: // Swap between distance and steps
-        if (dispmode == RAW){
+        if (dispmode == RAW)
+        {
             next_disp = MPS;     // edit here for doing things with buttons
-        } else {
-            next_disp= RAW;
+        }
+        else
+        {
+            next_disp = RAW;
         }
         break;
 
     case 1: // Swap between units (only works if it is displaying distance already)
-        if (dispmode == MPS){
+        if (dispmode == MPS)
+        {
             next_disp = GRAV;     // edit here for doing things with buttons
-        } else if (dispmode == GRAV){
+        }
+        else if (dispmode == GRAV)
+        {
             next_disp = MPS;
         }
         break;
@@ -320,3 +269,61 @@ uint8_t swap_disp(uint8_t dispmode, uint8_t dir)
     return next_disp;
 }
 
+/*
+ uint8_t butState;
+ int flag = 0;
+
+ // Poll the buttons
+ updateButtons();
+
+ // check state of each button
+ butState = checkButton(UP);
+ switch (butState)
+ {
+ case PUSHED: // increment dispMode cycler if a change is detected on UP button
+ if (dispMode == MPS) {
+ dispMode = RAW;
+ } else {
+ dispMode++;
+ }
+ break;
+ // Do nothing if state is NO_CHANGE or RELEASED
+ }
+ butState = checkButton(DOWN);
+ switch (butState)
+ {
+ case PUSHED: // set flag to logic high if a change is detected on DOWN button
+ flag = 1;
+ break;
+ // Do nothing if state is NO_CHANGE or RELEASED
+ }
+ */
+
+/*
+ butState = checkButton(LEFT);
+ switch (butState)
+ {
+ case PUSHED:
+ displayButtonState("LEFT ", "PUSH", ++leftPushes, 2);
+ break;
+ case RELEASED:
+ displayButtonState("LEFT ", "RELS", leftPushes, 2);
+ break;
+ // Do nothing if state is NO_CHANGE
+ }
+ butState = checkButton(RIGHT);
+ switch (butState)
+ {
+ case PUSHED:
+ displayButtonState("RIGHT", "PUSH", ++rightPushes, 3);
+ break;
+ case RELEASED:
+ displayButtonState("RIGHT", "RELS", rightPushes, 3);
+ break;
+ // Do nothing if state is NO_CHANGE
+ }
+ */
+
+// set each field of the vector_buttons    - Better to do this as they happen?
+//    button_flags.dispMode = dispMode;
+//    button_flags.D = flag;
--- 361Project/main.c
+++ 361Project/main.c
@@ -13,7 +13,7 @@
 #include <math.h>
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
-#include "inc/hw_ints.h"
+//#include "inc/hw_ints.h"
 #include "driverlib/gpio.h"
 #include "driverlib/uart.h"
 #include "driverlib/sysctl.h"
@@ -33,11 +33,10 @@
 #include "stepCounter.h" //
 
 // Define Variables
+#define SYS_DELAY_DIV 128 // divisor for main loop timing
 #define SAMPLE_RATE_HZ 100 // rate for sampling accelerometers
-#define HOLD_TIME 100 // amount of cycles the button needs to be held down to confirm
-
-
-
+#define HOLD_TIME 50 // amount of cycles the button needs to be held down to confirm
+#define DB_TIME 3 // number of cycles to debounce a button
 
 // Begin main loop
 int main(void)
@@ -55,35 +54,25 @@ int main(void)
     initButtons();
     initAccl();
 
-
     FPUEnable(); // enable FPU co-processor
     IntMasterEnable(); // Enable interrupts to the processor.
 
     // Declaring accel measurement variables and vars used upon startup for the initialisation calibration
     vector3_t mean_acc;
-    vector3_t offset;
-    vector3_t offset_mean_acc;
     uint32_t accel_norm;
     uint32_t steps; //
-    uint8_t acc_buf_filled = 0;
-    uint16_t init_cycles = 0;
 
     // declaring int variables and initialising values to 0
     uint32_t total_steps = 0;
     uint32_t less_than = 0;
     uint32_t prev_less_than = 0;
 
-    // Initialising offsets to 0
-    offset.x = 0;
-    offset.y = 0;
-    offset.z = 0;
-
     // Enable changing the displayed units
 //    uint8_t unitMode = UNITS_RAW;
-    vector_inputs inputFlags = {.D=0, .L=0, .R=0, .U=0};
+    vector_inputs inputFlags = { .D = 0, .L = 0, .R = 0, .U = 0 };
     uint8_t dispMode = RAW;
 
-    int held = 0;
+    uint8_t held = 0;
 
     while (1) // TODO: On SYSCLK interrupt raising main_flag, run this while loop.
     {
@@ -91,7 +80,7 @@ int main(void)
 
         // THIS IS CAUSING THE LONG PRESS BUG, when combined with SysCtlDelay elsewhere (ie in buttons4.c)
         // THIS IS BAD METHOD - need to use interrupts properly.
-        SysCtlDelay(SysCtlClockGet() / 128);    // (not) approx 2 Hz
+        SysCtlDelay(SysCtlClockGet() / SYS_DELAY_DIV);    // (not) approx 2 Hz
 
         // Check buttons and update mode if required
         inputFlags = readButtonFlags(inputFlags);
@@ -101,52 +90,28 @@ int main(void)
         // The mean is then calculated and stored in a separate 3D vector variable
         mean_acc = getAcclData();
 
-        // CALIBRATION OF ACCELEROMETERS
-        // Upon initialization, the device waits until the buffers have been
-        // filled and then uses the mean to set the first offset
-        if (acc_buf_filled == 0)
-        {
-            init_cycles++;
-            if (init_cycles > BUF_SIZE)
-            {
-                acc_buf_filled = 1;
-                offset = mean_acc;
-            }
-        }
-
-
-
-        // Where all the polls are handled
-        // If held down for an amount of time, the steps reset
-            if (inputFlags.D == 1)
-            {
-                held = detect_hold(DOWN, HOLD_TIME);
-                if (held == 1) {
-                    total_steps = 0;
-                    inputFlags.D = 0;
-                    // offset = mean_acc; we don't need to offset anymore
-                }
-            }
-            //swaps units in distance mode on up button press only if the distance is displayed
-            if (inputFlags.U == 1){ //Magic number change later
-                dispMode = swap_disp(dispMode, 1);
-                inputFlags.U = 0;
-            }
-
-            //swaps from step and distance modes on left and right button presses
-            if (inputFlags.R == 1 || inputFlags.L == 1){
-                dispMode = swap_disp(dispMode, 0); //Magic number change later
-                inputFlags.R = 0;
-                inputFlags.L = 0;
-            }
-
-
-
-        // Recalculates the current value by subtracting the current
-        // mean acceleration by the last saved offset
-        offset_mean_acc.x = mean_acc.x - offset.x;
-        offset_mean_acc.y = mean_acc.y - offset.y;
-        offset_mean_acc.z = mean_acc.z - offset.z;
+        // CALIBRATION OF ACCELEROMETERS // Unnecessary and makes for bad RMS if offset large
+//        vector3_t offset;
+//        vector3_t offset_mean_acc;
+//        uint8_t acc_buf_filled = 0;
+//        uint16_t init_cycles = 0;
+//
+//        // Upon initialization, the device waits until the buffers have been
+//        // filled and then uses the mean to set the first offset
+//        if (acc_buf_filled == 0)
+//        {
+//            init_cycles++;
+//            if (init_cycles > BUF_SIZE)
+//            {
+//                acc_buf_filled = 1;
+//                offset = mean_acc;
+//            }
+//        }
+//        // Recalculate the current value by subtracting the current
+//        // mean acceleration by the last saved offset
+//        offset_mean_acc.x = mean_acc.x - offset.x;
+//        offset_mean_acc.y = mean_acc.y - offset.y;
+//        offset_mean_acc.z = mean_acc.z - offset.z;
 
         // Calculate the norm of the x, y, and z acceleration readings in G's (could maybe make it a one argument function taking a vector3_t)
         accel_norm = calculate_norm(mean_acc); //
@@ -173,6 +138,51 @@ int main(void)
             displayMiles(steps);
             break;
         }
-    }
 
+        // Where all the polls are handled
+        // If held down for an amount of time, the steps reset
+        if (inputFlags.D == 1)
+        {
+            held = detect_hold(DOWN, HOLD_TIME);
+            if (held == 1)
+            {
+                total_steps = 0;
+                inputFlags.D = 0;
+                // offset = mean_acc; we don't need to offset anymore
+            }
+        }
+        //swaps units in distance mode on up button press only if the distance is displayed
+        if (inputFlags.U == 1)
+        { //Magic number change later
+            held = detect_hold(UP, DB_TIME);
+            if (held == 1)
+            {
+                dispMode = swap_disp(dispMode, 1);
+                inputFlags.U = 0;
+            }
+        }
+
+        //swaps from step and distance modes on left and right button presses
+        if (inputFlags.L == 1)
+        {
+            held = detect_hold(LEFT, DB_TIME);
+            if (held == 1)
+            {
+                dispMode = swap_disp(dispMode, 1);
+                inputFlags.L = 0;
+            }
+            dispMode = swap_disp(dispMode, 0); //Magic number change later
+        }
+
+        if (inputFlags.R == 1)
+        {
+            held = detect_hold(RIGHT, DB_TIME);
+            if (held == 1)
+            {
+                dispMode = swap_disp(dispMode, 1);
+                inputFlags.R = 0;
+            }
+            dispMode = swap_disp(dispMode, 0); //Magic number change later
+        }
+    }
 }
